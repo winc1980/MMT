869d04557c933f044413173438305ca6b8570fe4
nekokujira24wase <120014921+nekokujira24wase@users.noreply.github.com>
2024-03-01 20:31:42 +0900

    バックアップ

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  // basePath: '/docs',
+  // images : {
+  //   domain: [
+  //     ''
+  //   ]
+  // }
 }
 
 module.exports = nextConfig
